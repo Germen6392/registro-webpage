5b7daba5927fb401dbbcddb74d49cd1a0e5cc28e
Germen6392 <gerbarinas25@gmail.com>
2023-11-16 17:24:55 -0300

    props and styles added

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,22 @@ function App() {
     <>
     <nav>
       <ul>
-    <Navbar navItems="Inicio"/>
-    <Navbar navItems="Sobre Nosotros"/>
-    <Navbar navItems="Argentina Programa"/>
+        <Navbar navItems="Inicio"/>
+        <Navbar navItems="Sobre Nosotros"/>
+        <Navbar navItems="Argentina Programa"/>
       </ul>
     </nav>
     <h1>Ejercicio 6</h1> 
-    <Form/> 
+    <h2>Regístrate</h2>
+    <form>
+      <Form label="Nombre" inputType="text" placeholderValue="Nombre"/>
+      <Form label="Apellido" inputType="text" placeholderValue="Apellido"/>
+      <Form label="Email" inputType="email" placeholderValue="correo@correo.com"/>
+      <Form label="Teléfono" inputType="phone" placeholderValue="1234567890"/>
+      <Form label="Coloca una clave" inputType="password" placeholderValue=""/>
+      <Form label="Repite tu clave" inputType="password" placeholderValue=""/>
+      <button type='submit'>Enviar</button>
+    </form> 
     </>
   )
 }
--- src/assets/components/Form.jsx
+++ src/assets/components/Form.jsx
@@ -1,32 +1,15 @@
-function Form() {
+import './Form.css'
+function Form({label,inputType,placeholderValue}) {
     return (
         <>
-            <form action="">
-                <h2>Registrate</h2>
+                          
                 <fieldset>
-                    <legend>Nombre y Apellido</legend>
-                    <input type="text" placeholder="Nombre"/>
-                    <input type="text" placeholder="Apellido"/>
+                    <legend>{label}</legend>
+                    <input type={inputType} placeholder={placeholderValue}/>
+                    
                 </fieldset>
-                <fieldset>
-                    <legend>E-Mail</legend>
-                    <input type="email" name="" id="" placeholder="correo@correo.com"/>
-                </fieldset>
-                <fieldset>
-                    <legend>Teléfono</legend>
-                    <input type="tel" name="" id="" placeholder="1234567890"/>
-                </fieldset>
-                <fieldset>
-                    <legend>Registra tu contraseña</legend>
-                    <label htmlFor="">Clave</label>
-                    <input type="password" name="" id=""/>
-                    <label htmlFor="">Repite tu Clave</label>
-                    <input type="password" name="" id=""/>
-                </fieldset>
-                <fieldset>
-                    <button type="submit">Enviar</button>
-                </fieldset>
-            </form>
+                
+            
         </>
     )
 }
